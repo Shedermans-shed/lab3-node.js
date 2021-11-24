fef0b2685d923e3d504277884bcb4b0a9c8e1d7f
Shedermans-shed <91608668+Shedermans-shed@users.noreply.github.com>
2021-11-24 15:47:32 +0000

    up to q5

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
 const express = require("express");
 //creating app const
 const app = express();
-//send the index.html when receiving HTTP GET /
+
+//handling static HTML and EJS templates
+app.use(express.static("public"));
+app.set("view engine", "ejs");
 app.get("/", (req, res) => {
-  res.sendFile("public/index.html", { root: __dirname });
+  res.render("index"); //no need for ejs extension
 });
 
 //make the app listen on port
 const port = process.argv[2] || process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log("Cart app listening at http://localhost:${port}");
+});
+
+//route for contacts
+app.get("/contacts", (req, res) => {
+  res.render("contacts");
 });
